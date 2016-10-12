3fe0826b747c69b2c8df271a1d1cdd651bc390c6
katajam <yigeeagle@gmail.com>
2016-10-12 00:14:10 -0600

    add prefabs and scripts for forest blocks

---
+++ NullReferenceException/Assets/Scripts/Platforms/LeftRightPlatform.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftRightPlatform : MonoBehaviour {
+
+	public float MovingSpeed = 2f;
+	public Vector2 Direction = Vector2.right;
+	public float Distance = 3f;
+	public float StartPosition;
+
+	void Start(){
+		StartPosition = transform.position.x;
+	}
+
+	void FixedUpdate() {
+
+		var CurrentPosition = transform.position.x;
+
+		if (CurrentPosition > StartPosition + Distance) {
+			Direction = -Direction;
+		}
+
+		if (CurrentPosition < StartPosition) {
+			Direction = Vector2.right;
+		}
+
+		transform.Translate (Direction * MovingSpeed * Time.deltaTime);
+
+	}
+}
