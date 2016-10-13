cd69b65088685fe52ab01b0b262e6c6ce2c0c189
Dylan P <Derbstep@gmail.com>
2016-10-13 00:07:53 -0600

    Path movement

---
+++ NullReferenceException/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MoveObject : MonoBehaviour {
+	[SerializeField] private Path path;
+	[SerializeField] private float Speed = 3.0f;
+	[SerializeField] private float MaxDistanceToGoal = 0.1f;
+
+	private IEnumerator<Transform> _curPoint;
+
+	public void Start(){
+		if (path == null) {
+			return;
+		}
+
+		_curPoint = path.GetEnumerator ();
+		_curPoint.MoveNext ();
+
+		if (_curPoint.Current == null)
+			return;
+
+		transform.position = _curPoint.Current.position;
+	}
+
+	public void Update(){
+		if (_curPoint == null || _curPoint.Current == null)
+			return;
+
+		transform.position = Vector3.MoveTowards (transform.position, _curPoint.Current.position, Time.deltaTime * Speed);
+
+		if ((transform.position - _curPoint.Current.position).sqrMagnitude < MaxDistanceToGoal * MaxDistanceToGoal)
+			_curPoint.MoveNext ();
+	}
+
+}
---
+++ NullReferenceException/Assets/Scripts/Path.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Path : MonoBehaviour {
+	[SerializeField] private Transform[] Points;
+
+	public void OnDrawGizmos(){
+		if (Points == null || Points.Length < 2)
+			return;
+
+		for (int i = 0; i < Points.Length-1; i++) {
+			Gizmos.DrawLine (Points [i].position, Points [i + 1].position);
+		}
+	}
+
+	public IEnumerator<Transform> GetEnumerator(){
+		if (Points == null || Points.Length < 1)
+			yield break;
+
+		int direction = 1;
+		int i = 0;
+
+		while (true) {
+			yield return Points[i];
+
+			if (Points.Length == 1)
+				continue;
+
+			if (i <= 0) {
+				direction = 1;
+			} else if (i >= Points.Length - 1) {
+				direction = -1;
+			}
+
+			i += direction;
+		}
+	}
+}
