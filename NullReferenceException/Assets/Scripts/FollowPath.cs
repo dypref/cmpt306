f46b261ae4564456776e03721eff9037933cd700
Dylan P <Derbstep@gmail.com>
2016-10-13 14:43:03 -0600

    Follow Path Commenting

--- NullReferenceException/Assets/Scripts/FollowPath.cs
+++ NullReferenceException/Assets/Scripts/FollowPath.cs
@@ -9,16 +9,20 @@ public class FollowPath : MonoBehaviour {
 	private IEnumerator<Transform> _curPoint;
 
 	public void Start(){
+		// Need a path to do anything
 		if (path == null) {
 			return;
 		}
 
+		// Get the Enumerator
 		_curPoint = path.GetEnumerator ();
+		// Get the location of the initial point
 		_curPoint.MoveNext ();
 
 		if (_curPoint.Current == null)
 			return;
 
+		// Move to the initial point
 		transform.position = _curPoint.Current.position;
 	}
 
@@ -26,8 +30,10 @@ public class FollowPath : MonoBehaviour {
 		if (_curPoint == null || _curPoint.Current == null)
 			return;
 
+		// Move towards the next point
 		transform.position = Vector3.MoveTowards (transform.position, _curPoint.Current.position, Time.deltaTime * Speed);
 
+		// If we reach the destination point, move on to the next
 		if ((transform.position - _curPoint.Current.position).sqrMagnitude < MaxDistanceToGoal * MaxDistanceToGoal)
 			_curPoint.MoveNext ();
 	}
