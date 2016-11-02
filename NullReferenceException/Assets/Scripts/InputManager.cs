1494648e1b66e4fe1749e944adb1cce7a8b1a44b
yih877 <yih877@mail.usask.ca>
2016-11-02 01:08:39 -0600

    Add functionalities: tap to jump and tilt to move

---
+++ NullReferenceException/Assets/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public ChumpyMovement Chumpy;
+	public GameObject Player;
+
+	void Start() {
+		// Enable gyro
+		Input.gyro.enabled = true;
+
+		// Get Access to Chumpy Movement Script
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		Chumpy = Player.GetComponent<ChumpyMovement> ();
+	}
+
+	void Update(){
+
+		// Let Chumpy jump by tapping on the touch screen
+		if (Input.GetMouseButtonDown(0)) {
+			Chumpy.Jump ();
+		}
+
+		// Let Chumpy move by tilting the phone at any angle
+		float initialOrientationX = Input.gyro.rotationRateUnbiased.x;
+		float initialOrientationY = Input.gyro.rotationRateUnbiased.y;
+		Chumpy.Move (initialOrientationX, initialOrientationY);
+
+	}
+
+}
