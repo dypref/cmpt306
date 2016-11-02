0593ae4b5c693ea931a37d0f04a8a93e9e058c82
yih877 <yih877@mail.usask.ca>
2016-11-02 01:09:20 -0600

    Add mobile part on ChumpyMovement Script

--- NullReferenceException/Assets/Scripts/ChumpyMovement.cs
+++ NullReferenceException/Assets/Scripts/ChumpyMovement.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 public class ChumpyMovement : MonoBehaviour {
 	// Movement speed for this unit
@@ -18,7 +20,7 @@ public class ChumpyMovement : MonoBehaviour {
 
 	void Start(){
 		// Instantiate Chumpy's GameObjects
-		_animator = GetComponent<Animator> ();
+		// _animator = GetComponent<Animator> ();
 		_rb2d = GetComponent<Rigidbody2D> ();
 
 		// Move chumpy to the spawn
@@ -45,6 +47,18 @@ public class ChumpyMovement : MonoBehaviour {
 		// Jump if chumpy is on the ground
 		if (_isGrounded && Input.GetKey (KeyCode.Space))
 			_rb2d.AddForce (new Vector2 (0, JumpPower));
+
+	}
+
+	// Jump for InputManager Script on mobile device
+	public void Jump() {
+		if (_isGrounded)
+			_rb2d.AddForce (new Vector2 (0, JumpPower*5));
+	}
+
+	// Move for InputManager Script on mobile device
+	public void Move(float x, float y) {
+		_rb2d.AddForce (new Vector2 (y * MovementSpeed, -x * MovementSpeed));
 	}
 
 	void Respawn(){
@@ -61,4 +75,5 @@ public class ChumpyMovement : MonoBehaviour {
 	Vector2 GetGroundCheck(){
 		return new Vector2 (_rb2d.position.x, _rb2d.position.y - 0.5f);
 	}
+		
 }
