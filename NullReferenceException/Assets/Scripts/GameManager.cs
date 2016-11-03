2bf585aed6d4597a43905417fdabfb70254cc676
yih877 <yih877@mail.usask.ca>
2016-11-03 00:48:30 -0600

    Make exit button work

---
+++ NullReferenceException/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Exit the game, for the exit button on the main menu
+	public void doExitGame() {
+		Application.Quit();
+	}
+}
