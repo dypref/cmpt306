440a251023a7397a6571f7358e320b9071d45ff3
Dylan P <Derbstep@gmail.com>
2016-11-02 23:48:27 -0600

    Bouncy Platform

---
+++ NullReferenceException/Assets/Scripts/Platforms/Bouncy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bouncy : MonoBehaviour {
+	[SerializeField] private float BounceForce = 100.0f;
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.CompareTag("Player")){
+			other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, BounceForce));
+		}
+	}
+}
