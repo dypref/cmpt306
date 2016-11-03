198554350f6063b9ef97ca64bb1a0331b39c6450
Dylan P <Derbstep@gmail.com>
2016-11-02 23:42:32 -0600

    Dissapearing(after touch) platform script

---
+++ NullReferenceException/Assets/Scripts/Platforms/Dissapearing.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dissapearing : MonoBehaviour {
+	[SerializeField] private float DissapearAfter = 2.0f;
+
+	private bool _dissapearing;
+
+	void Start(){
+		_dissapearing = false;
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (!_dissapearing && other.gameObject.CompareTag ("Player")) {
+			_dissapearing = true;
+			Invoke ("DestroyThis", DissapearAfter);
+		}
+	}
+
+	void DestroyThis(){
+		Destroy (gameObject);
+	}
+}
