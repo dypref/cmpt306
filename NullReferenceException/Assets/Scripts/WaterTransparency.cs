c877f80e5db774da3dd90db1726cd8994543f095
Kevin Noonan <kevinnooonan@gmail.com>
2016-11-03 13:44:57 -0600

    Adding Stuff

---
+++ NullReferenceException/Assets/Scripts/WaterTransparency.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterTransparency : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Color transparent = GetComponent<SpriteRenderer>().color;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, .7f);
+	}
+}
