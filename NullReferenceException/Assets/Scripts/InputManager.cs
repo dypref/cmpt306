ffcbc8e91734316b04ef64c8149b9266c49480b7
yih877 <yih877@mail.usask.ca>
2016-11-06 08:45:06 -0600

    Added ChangeScene script to help enter into next level

--- NullReferenceException/Assets/Scripts/InputManager.cs
+++ NullReferenceException/Assets/Scripts/InputManager.cs
@@ -2,20 +2,22 @@
 using System.Collections;
 
 public class InputManager : MonoBehaviour {
+	
 	[SerializeField] private bool KeyboardInput = true;
 
-	public ChumpyMovement Chumpy;
+	private ChumpyMovement Chumpy;
 
 	void Start() {
 		Chumpy = GetComponent<ChumpyMovement> ();
 	}
 
 	void Update(){
-		// Let Chumpy jump by tapping on the touch screen
+		// Let Chumpy jump by tapping on the touch screen or pressing space bar
 		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown (KeyCode.Space)) {
 			Chumpy.Jump ();
 		}
 
+		// Let Chumpy move by taking input from the keyboard or the device
 		float moveX = (KeyboardInput) ? Input.GetAxis("Horizontal") : Input.acceleration.x;
 
 		Chumpy.Move (moveX, 0);
