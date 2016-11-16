f40b989ab99bbf31ef46209e828581e346e34e5c
yih877 <yih877@mail.usask.ca>
2016-11-16 01:51:25 -0600

    Added functionality: back button to exit game.

--- NullReferenceException/Assets/Scripts/ExitGame.cs
+++ NullReferenceException/Assets/Scripts/ExitGame.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class ExitGame : MonoBehaviour {
 
+	// Exit the game, for the back button on the android phone
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			doExitGame ();
+		}
+	}
+
 	// Exit the game, for the exit button on the main menu
 	public void doExitGame() {
 		Application.Quit();
