0ac4be2dc800e7402adb69cb924dc2ec3935d09f
yih877 <yih877@mail.usask.ca>
2016-11-16 01:29:48 -0600

    Added global music setting to Setting scene and put shadows on all exist scenes.

---
+++ NullReferenceException/Assets/Scripts/MusicSetting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MusicSetting : MonoBehaviour {
+
+	// Music icons for on and off
+	[SerializeField]
+	public Sprite MusicOn;
+	[SerializeField] 
+	public Sprite MusicOff;
+
+	// Two states for music setting: on and off
+	private enum MusicState {
+		Off,
+		On
+	};
+
+	// For Setting scene to turn on/off music and sound effects
+	public void MusicToggle() {
+		
+		if(AudioListener.volume == (float)(int)MusicState.On) {
+			AudioListener.volume = (float)(int)MusicState.Off;
+			GetComponent<Image> ().sprite = MusicOff;
+		}
+		else if(AudioListener.volume == (float)(int)MusicState.Off) {
+			AudioListener.volume = (float)(int)MusicState.On;
+			GetComponent<Image> ().sprite = MusicOn;
+		}
+	}
+}
