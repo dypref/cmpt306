2acebce4ea59b27cd55f74afda4657c57bc911d0
yih877 <yih877@mail.usask.ca>
2016-11-22 08:43:37 -0600

    Added swipe gesture to swipe between scenes, and part of other stuff.

---
+++ NullReferenceException/Assets/Scripts/LevelSelectManager/Level.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Level {
+
+	// The index of current level.
+	public int index;
+	// Is this level locked or not?
+	public bool isLocked;
+	// How many coins you get?
+	public int starCount;
+	// Is this level finished?
+	public bool isFinished;
+
+	public SpriteRenderer level;
+
+	void Start () {
+		isLocked = true;
+		starCount = 0;
+		isFinished = false;
+	}
+
+}
---
+++ NullReferenceException/Assets/Scripts/SwipeHandler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SwipeHandler : MonoBehaviour {
+
+	private float swipeDistance = 0;
+	private float swipeStandard = 20f;
+	private Vector3 initialPosition = Vector3.zero;
+	private float fingerX = 0;
+	private float fingerY = 0;
+
+	public String leftScene;
+	public String rightScene;
+
+	void Awake() {
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Update() {
+		
+		if (Input.GetMouseButtonDown(0)) {
+			initialPosition = Input.mousePosition;
+
+		} else if (Input.GetMouseButton(0)) {
+			fingerX = initialPosition.x - Input.mousePosition.x;
+			fingerY = initialPosition.y - Input.mousePosition.y;
+			swipeDistance = Mathf.Sqrt(Mathf.Pow(fingerX, 2) + Mathf.Pow(fingerY, 2));
+		
+		} else if (Input.GetMouseButtonUp(0)) {
+			if (swipeDistance > swipeStandard) {
+				if (fingerX > 0) {
+					SceneManager.LoadScene (leftScene);
+				} else {
+					SceneManager.LoadScene (rightScene);
+				}
+			}
+
+			fingerX = 0;
+			fingerY = 0;
+			initialPosition = Vector3.zero;
+		}
+	}
+	
+
+}
