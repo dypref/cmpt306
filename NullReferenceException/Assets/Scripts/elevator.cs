d5579f1e3d898949f2384885845caa754cccb32e
Kevin Noonan <kevinnooonan@gmail.com>
2016-12-01 12:31:33 -0600

    level 14 bugtested + script added

---
+++ NullReferenceException/Assets/Scripts/elevator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class elevator : MonoBehaviour {
+
+    public Transform teleport;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if(player.tag == "Player")
+        {
+            player.transform.position = teleport.position;
+        }
+    }
+}
