9ad03837f4c3704ef873eb190d9bffbced2813ad
Kevin Noonan <kevinnooonan@gmail.com>
2016-12-01 11:28:48 -0600

    Level 13 bugtested + script added

---
+++ NullReferenceException/Assets/Scripts/BuildingTransparency.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingTransparency : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D building)
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    void OnTriggerExit2D(Collider2D building)
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
