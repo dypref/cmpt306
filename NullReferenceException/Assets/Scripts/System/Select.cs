73a9ca879f4d6343d4e344b6527d661634eefb3c
IxLahiji <ixl164@usask.ca>
2016-12-08 12:53:17 -0600

    Fixed camera stuff in all levels, and linking

--- NullReferenceException/Assets/Scripts/System/Select.cs
+++ NullReferenceException/Assets/Scripts/System/Select.cs
@@ -7,5 +7,8 @@ public class Select : MonoBehaviour {
     public void LoadScene(string SceneName) {
         SceneManager.LoadScene(SceneName);
     }
-	
+
+    public void ReloadScene() {
+        LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
